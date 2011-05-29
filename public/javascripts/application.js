dc713c2f2f508af7d9ac0fbe73991843b5c468b2
Ammo Goettsch <ammo@derammo.net>
2011-05-29 11:20:01 -0400

    moved application javascript for tabbing to the correct framework-provided file

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function activateTab(pageId) {
+	var tabCtrl = document.getElementById('tabCtrl');
+	var pageToActivate = document.getElementById(pageId);
+	for ( var i = 0; i < tabCtrl.childNodes.length; i++) {
+		var node = tabCtrl.childNodes[i];
+		if (node.nodeType == 1) { /* Element */
+			node.style.display = (node == pageToActivate) ? 'block' : 'none';
+		}
+	}
+}
