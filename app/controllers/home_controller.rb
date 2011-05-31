6e70a274a490c46292dbf66d507a23362619f327
Ammo Goettsch <ammo@derammo.net>
2011-05-31 11:50:27 -0400

    added some explanations for contents of pages and context-sensitive instructions on the home page
    cleaned up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,11 @@ class HomeController < ApplicationController
   layout 'general'
   
   def index
+    if current_user then
+      @playerexists = Player.all(:conditions=> ['LOWER(owner) = ?', current_user.login]).count > 0
+    else
+      @playerexists = false
+    end
   end
 
 end
